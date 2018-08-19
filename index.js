898e1a361e5f49e24c0aef24dfd207c4b3a76ace
Jem Bezooyen <jem@hipmedia.ca>
2018-08-19 16:09:10 -0700

    Create an index.js file and log a location for now
    Just for quick testing: I threw in a console log and then experimented
    with a bash function that would take that output and change directory
    with it. So far it works!

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// For now we'll just travel to the root directory of this project
+console.log('~/Sites/misc/navi/')
+
+/*
+
+Then I add the following to the .zshrc to trigger the function
+and travel over to wherever this function spits out
+
+function navi () {
+  PATHTOVISIT=$(/Users/jem/Sites/misc/navi/index.js)
+  PREVIOUS_DIR=$(pwd)
+  eval "cd $PATHTOVISIT"
+}
+
+*/
+
+
